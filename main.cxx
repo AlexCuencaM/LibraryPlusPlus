3b7e0399a16d12f4e7ffa90c2f027b9ffcde0c78
Alex Cuenca M <maxpowerxd8y9@gmail.com>
2025-07-16 21:44:25 -0500

    First entity

--- main.cxx
+++ main.cxx
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <Entities/Book.h>
 using namespace std;
 int main(int argc, char* argv[]) 
 {
-    cout << "Hello, group!" << endl;
+    Book myBook;
+    myBook.Title = "1984";
+    myBook.Author = "George Orwell";
+    myBook.Publisher = "Secker & Warburg";
+    myBook.Year = 1949;
+    myBook.Pages = 328;
+    myBook.ISBN = "978-0451524935";
+    cout << "Book Title: " << myBook.Title << endl;
+    cout << "Author: " << myBook.Author << endl;
+    cout << "Publisher: " << myBook.Publisher << endl;
+    cout << "Year: " << myBook.Year << endl;
+    cout << "Pages: " << myBook.Pages << endl;
+    cout << "ISBN: " << myBook.ISBN << endl;
     return 0;
 }
