f966ae631711d04b2ebb45feed9c02750a16e2cb
Alex Cuenca <alex.cuenca@expotuna.com>
2025-07-31 17:56:39 -0500

    Deleting test method

--- main.cxx
+++ main.cxx
@@ -17,59 +17,12 @@
 // using namespace std;
 int main(int argc, char* argv[]) 
 {
-    // Initialize the linked lists for books and users
+    // Initialize the data structures for books and users
     ModernLinkedList<Book> bookList;
     ModernLinkedList<User> userList;
     ModernLinkedList<LoanBook> loanBookList;
     ModernQueue<Node<Book>*> books;
     BinaryTreeImpl<Book> tree(books);
-    // Mock data for books
-    // Book book1;
-    // book1.Title = "Hola";
-    // book1.Author = "Author1";
-    // book1.Year = 2020;
-    // book1.ISBN = "ISBN001";
-
-    // Book book2;
-    // book2.Title = "Adios";
-    // book2.Author = "Author2";
-    // book2.Year = 2021;
-    // book2.ISBN = "ISBN002";
-
-    // Book book3;
-    // book3.Title = "Bienvenido";
-    // book3.Author = "Author3";
-    // book3.Year = 2022;
-    // book3.ISBN = "ISBN003";
-    // Book book4;
-    // book4.Title = "Chocolate oe";
-    // book4.Author = "Author3";
-    // book4.Year = 2022;
-    // book4.ISBN = "ISBN003";
-
-    // tree.insertNode(book1);
-    // tree.insertNode(book2);
-    // tree.insertNode(book3);
-    // tree.insertNode(book4);
-    // // Add mock books to the linked list and queue
-    // string title = "Chocolate oe";
-    // Book* bookMoment = tree.firstOrDefault([title](Book currentBook) ->
-    //     bool{
-    //         return currentBook.Title == title;
-    //     });
-
-    // bool isFound = tree.any([title](Book currentBook) -> 
-    //     bool { 
-    //         return currentBook.Title == title;
-    //     });
-    // if(isFound){
-    //     cout << "Boook found !!" << endl;
-    //     cout << *bookMoment << endl;
-    // }
-    // else {
-    //     cout << "Boook not found :(" << endl;
-    // }
-    
     // Initialize the services
     BookService bookService(bookList, tree);
     UserService userService(userList); 
